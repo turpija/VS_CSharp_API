a65acf6555b12663e4f9c6e4401c2f938f0f5cdd
turpija <turpija@gmail.com>
2023-04-04 23:45:51 +0200

    expenseView, required annotation

--- Budget.MVC/Models/ExpenseView.cs
+++ Budget.MVC/Models/ExpenseView.cs
@@ -9,13 +9,26 @@ namespace Budget.MVC.Models
     public class ExpenseView
     {
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
         public string Name { get; set; }
+
         public string Description { get; set; }
+
+        [Required]
         public Guid PersonId { get; set; }
+
+        [Required]
         public Guid CategoryId { get; set; }
+
+        [Required]
         public DateTime Date { get; set; }
+
+        [Required]
         public decimal Cost { get; set; }
+
         public PersonView Person { get; set; }
+       
         public CategoryView Category { get; set; }
     }
 }
